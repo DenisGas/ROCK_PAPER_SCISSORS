9ec0bc60a755341d718d12e515f4966516d6b991
DenisGas <dehuc1811@gmail.com>
2022-06-24 18:42:49 +0300

    Added round logic

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ function computerPlay(random){
   return computerSelection =  figureArray[random];
 }
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection){
+    return ('NO Winner')
+  }else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    plrWns++
+    return ("Player Win, Paper beats Rock")
+  }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+    pcWns++
+    return ("PC Win, Scissors beats Rock")
+  }else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+    plrWns++
+    return ("Player Win, Rock beats Scissors")
+  }else if (playerSelection === 'rock' && computerSelection === 'paper'){
+    pcWns++
+    return ("PC Win, Paper beats Rock")
+  }else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+    plrWns++
+    return ("Player Win, Scissors beats Paper")
+  }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+    pcWns++
+    return ("PC Win, Rock beats Scissors")
+}return("ERROR")
+}
