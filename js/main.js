01f25d42e8dde0037d04f794c2ead1b2dc98b627
DenisGas <dehuc1811@gmail.com>
2022-06-24 21:32:46 +0300

    Added check playerSelection and edited some text and logic

--- js/main.js
+++ js/main.js
@@ -10,45 +10,58 @@ function computerPlay(random){
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection){
-    return ('NO Winner')
+    return ('Draw')
   }else if (playerSelection === 'paper' && computerSelection === 'rock'){
-    plrWns++
-    return ("Player Win, Paper beats Rock")
+    plrWns++;
+    return ("Player Win, Paper wraps Rock")
   }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-    pcWns++
-    return ("PC Win, Scissors beats Rock")
+    pcWns++;
+    return ("PC Win, Scissors cuts Paper")
   }else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-    plrWns++
+    plrWns++;
     return ("Player Win, Rock beats Scissors")
   }else if (playerSelection === 'rock' && computerSelection === 'paper'){
-    pcWns++
-    return ("PC Win, Paper beats Rock")
+    pcWns++;
+    return ("PC Win, Paper wraps Rock")
   }else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-    plrWns++
-    return ("Player Win, Scissors beats Paper")
+    plrWns++;
+    return ("Player Win, Scissors cuts Paper")
   }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-    pcWns++
+    pcWns++;
     return ("PC Win, Rock beats Scissors")
-}return("ERROR")
+  }
+  return("ERROR")
 }
 
 function game() {
   plrWns = 0;
   pcWns = 0;
   for (let i = 0; i < 5; i++) {
-   playerSelection = prompt('rock paper scissors', 'rock');
+   playerSelection = prompt('Choose Rock, Paper, Scissors', 'Rock');
+   playerSelectionCheck();
    computerSelection = computerPlay(getRandomInt(3));
    alert(playRound(playerSelection, computerSelection));
   }
-  let SCORE = ("PLR = " + plrWns + " PC = " + pcWns  + " NO WINS = " + (5 - (plrWns + pcWns)));
+  let SCORE = ("PLR = " + plrWns + ", PC = " + pcWns  + ", Draws = " + (5 - (plrWns + pcWns)));
   if(plrWns === pcWns){
       return(alert("NO WINNERS, Score: " + SCORE))
     }else if(plrWns > pcWns){
-      return(alert("Player Win Score: " + SCORE))
+      return(alert("Player Win, Score: " + SCORE))
     }else{
-      return(alert("PC Win Score: " + SCORE ))
+      return(alert("PC Win, Score: " + SCORE ))
     }
   
 }
 
 game()
+
+
+
+function playerSelectionCheck(){
+  if ((playerSelection) == null ||((playerSelection.toLowerCase()) != "rock" && (playerSelection.toLowerCase()) != "paper" &&(playerSelection.toLowerCase()) != "scissors") ){
+    playerSelection = prompt('Oops, Something went wrong!!! Choose Rock, Paper or Scissors', 'Rock');
+    playerSelectionCheck();
+   }else{
+    playerSelection = playerSelection.toLowerCase();
+   }
+}
